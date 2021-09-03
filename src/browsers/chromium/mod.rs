1c981618413f797de15ad9aac7152ac5ff6d124a
rumblefrog <contact@zhenyangli.me>
2021-09-03 19:58:52 -0400

    chore - aes for mac

--- src/browsers/chromium/mod.rs
+++ src/browsers/chromium/mod.rs
@@ -5,6 +5,9 @@ use super::Credential;
 #[cfg(target_os = "windows")]
 mod win;
 
+#[cfg(target_os = "macos")]
+mod macos;
+
 pub fn search_login_credentials(url: &str) -> ExtractorResult<Vec<Credential>> {
     #[cfg(target_os = "windows")]
     win::search_login_credentials(url)
