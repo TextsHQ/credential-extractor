d3f97df1a5385b462ca7d6e11bd13ddcaf7425df
rumblefrog <contact@zhenyangli.me>
2021-09-03 02:40:00 -0400

    fix - conditional build

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    #[cfg(target_os = "windows")]
     windows::build!(
         Windows::Win32::Security::CryptUnprotectData,
         Windows::Win32::Security::Cryptography::Core::CRYPTOAPI_BLOB,
