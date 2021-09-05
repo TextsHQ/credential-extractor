bf06fe83fe64d6f2fc6583eb9c8205730d2187b4
rumblefrog <contact@zhenyangli.me>
2021-09-05 08:37:19 -0400

    wip - local state super struct

--- src/browsers/chromium/mod.rs
+++ src/browsers/chromium/mod.rs
@@ -1,3 +1,5 @@
+use serde::Deserialize;
+
 use crate::error::ExtractorResult;
 
 use super::Credential;
@@ -8,10 +10,28 @@ mod win;
 #[cfg(target_os = "macos")]
 mod macos;
 
+#[derive(Deserialize)]
+pub struct LocalState {
+    #[cfg(target_os = "windows")]
+    pub os_crypt: OSCrypt,
+
+    // TODO: Profile (at least on Windows) is nested under profiles.info_cache
+}
+
+#[cfg(target_os = "windows")]
+#[derive(Deserialize)]
+pub struct OSCrypt {
+    pub encrypted_key: String,
+}
+
 pub fn login_credentials() -> ExtractorResult<Vec<Credential>> {
     #[cfg(target_os = "windows")]
     return win::login_credentials();
 
     #[cfg(target_os = "macos")]
     return macos::login_credentials();
+}
+
+pub(crate) fn browser_data() {
+
 }
