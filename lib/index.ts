acfe034412edbf71b9d5e35e7b16cc2bbf3bc5c5
rumblefrog <contact@zhenyangli.me>
2021-09-18 07:42:33 -0400

    feat - potential autofill elements

--- lib/index.ts
+++ lib/index.ts
@@ -3,14 +3,19 @@ const {
     decryptCredential,
 } = require('../ce.node');
 
+// TODO: Generic-ify Credential once non-browser sources are added
 export interface Credential {
     readonly browser: string;
 
     readonly url: string;
 
-    readonly username: string;
+    readonly username?: string;
 
     readonly encrypted_password: Buffer;
+
+    readonly username_element?: string;
+
+    readonly password_element?: string;
 }
 
 export function browserLoginCredentials(url: string): Credential[] {
