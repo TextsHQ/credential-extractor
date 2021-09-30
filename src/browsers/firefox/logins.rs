66c4f06d82034d5dd99a771cd25129ac3f281400
rumblefrog <contact@zhenyangli.me>
2021-09-30 14:56:54 -0400

    wip - logins file struct

---
+++ src/browsers/firefox/logins.rs
@@ -0,0 +1,39 @@
+use serde::Deserialize;
+
+pub struct LoginsFile {
+    #[serde(rename = "nextId")]
+    pub next_id: u32,
+}
+
+pub struct Login {
+    pub id: u32,
+
+    pub hostname: String,
+
+    #[serde(rename = "httpRealm")]
+    pub http_realm: Option<String>,
+
+    #[serde(rename = "formSubmitURL")]
+    pub form_submit_url: String,
+
+    #[serde(rename = "usernameField")]
+    pub username_field: Option<String>,
+
+    #[serde(rename = "passwordField")]
+    pub password_field: Option<String>,
+
+    #[serde(rename = "encryptedUsername")]
+    pub encrypted_username: String,
+
+    #[serde(rename = "encryptedPassword")]
+    pub encrypted_password: String,
+
+    #[serde(rename = "encType")]
+    pub enc_type: u64,
+
+    #[serde(rename = "timeCreated")]
+    pub time_created: u64,
+
+    #[serde(rename = "timeLastUsed")]
+    pub time_last_used: u64,
+}
