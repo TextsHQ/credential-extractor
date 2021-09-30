b175b152850ad8a6ec94d3cfe7a0e8ce999b40c8
rumblefrog <contact@zhenyangli.me>
2021-09-30 14:58:29 -0400

    wip - known browsers

---
+++ src/browsers/firefox/browsers.rs
@@ -0,0 +1,12 @@
+pub const KNOWN_BROWSER: &[FirefoxBrowser] = &[
+    FirefoxBrowser {
+        name: "Firefox",
+        paths: &["Mozilla", "Firefox"],
+    },
+];
+
+pub struct FirefoxBrowser {
+    pub name: &'static str,
+
+    pub paths: &'static [&'static str],
+}
